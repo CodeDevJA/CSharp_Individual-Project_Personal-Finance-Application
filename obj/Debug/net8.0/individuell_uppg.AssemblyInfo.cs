35ee2fbdb9457e8d8eef2ab8ecc86407b26a6681
CodeDevJA <codedev.ja@gmail.com>
2024-10-17 12:51:50 +0200

    skapar en sub-Class för ExpenseTransactionn och som ärver från Base-Classen TransactionBase, som ska generera object och ska anropa override Metoder

--- obj/Debug/net8.0/individuell_uppg.AssemblyInfo.cs
+++ obj/Debug/net8.0/individuell_uppg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("individuell_uppg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26e69d72511a5c2bdc54705656a0d96df8075ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36904b36e00b581ac5342fd2c86fad453c3f9191")]
 [assembly: System.Reflection.AssemblyProductAttribute("individuell_uppg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("individuell_uppg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
